4c9af94ea6fe6b9f5e42211a9a31e4b9cb3a7b20
Khaja Takreem UlLa <78692ishaq@gmail.com>
2022-08-20 10:31:06 +0530

    sails new app

---
+++ api/controllers/HomepageController.js
@@ -0,0 +1,14 @@
+/**
+ * HomepageController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+    homepage: function(req, res){
+        return res.view('homepage')
+    }
+};
+
